2b35b8a3c599304fa38f7281feb2d7a6a9d323c6
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-05-30 10:30:19 +0200

    Create Middleware admin.js

--- server/middleware/admin.js
+++ server/middleware/admin.js
@@ -1,6 +1,6 @@
 let admin = (req, res, next) =>{
-    if (req.body.role == 0){
-        res.send("You are not allowed");
+    if (req.user.role === 0){
+        return res.send("You are not allowed");
     }
 
     next()
